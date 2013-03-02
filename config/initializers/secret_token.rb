5fd02b1b5db859e54ebb0ecbdf379b6903d9aded
Phil Gengler <phil@pgengler.net>
2013-03-02 18:30:51 -0500

    'rails new bookmarks'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarks::Application.config.secret_token = '5c4e5681f57704f23ed201f8aa4d52168a5f3cc6ef5244eea974ac30893224aca38105d8ccbea8a07bf16bacbfdcd6491e409e602cce265913e5cda2f40d6f63'
