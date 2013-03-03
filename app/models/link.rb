535eefe14b3fbaa6a9f1d457501bf5edff607e34
Phil Gengler <phil@pgengler.net>
2013-03-03 15:09:01 -0500

    Make 'url' required for links

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ActiveRecord::Base
   attr_accessible :title, :url
 
 	validates :title, :presence => true
+	validates :url, :presence => true
 end
--- test/unit/link_test.rb
+++ test/unit/link_test.rb
@@ -3,9 +3,14 @@ require 'test_helper'
 class LinkTest < ActiveSupport::TestCase
 
 	test "'title' is required" do
-
 		assert_raises ActiveRecord::RecordInvalid do
 			Link.create! :url => 'http://example.com'
+		end
+	end
+
+	test "'url' is required" do
+		assert_raises ActiveRecord::RecordInvalid do
+			Link.create! :title => 'Dummy'
 		end
 	end
 
