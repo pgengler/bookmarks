0df39867bbb14b296c565524fb5763043df4c3f5
Phil Gengler <phil@pgengler.net>
2013-03-03 15:17:41 -0500

    Generate model for sections

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130303194206) do
+ActiveRecord::Schema.define(:version => 20130303201605) do
 
   create_table "links", :force => true do |t|
     t.string   "title"
     t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "sections", :force => true do |t|
+    t.string   "name"
+    t.integer  "position"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
