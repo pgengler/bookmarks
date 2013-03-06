ff62db75ed74fc859f7c119e85f406fc5ab44270
Phil Gengler <phil@pgengler.net>
2013-03-06 12:00:13 -0500

    Generate some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+	Section.create! name: "Section #{i}", position: i+1
+end
